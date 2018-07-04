9aa0204f6587c3a6a07e7e8707f83377604d2ba4
lovelife10000 <1358180015@qq.com>
2018-07-04 09:49:02 +0800

    update

---
+++ src/components/Child/index.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+
+class Child extends Component {
+    constructor(props) {
+        super(props)
+debugger
+
+    }
+
+
+    render() {
+debugger
+
+        return (
+            <div>
+                666
+            </div>
+
+        )
+    }
+}
+
+export default Child
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,79 +1,87 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import { bindActionCreators } from 'redux'
+import {bindActionCreators} from 'redux'
 import * as Actions from 'src/actions'
-import { withRouter } from 'react-router-dom'
-import { isLogin } from 'src/utils/auth'
-import { Form, Icon, Input, Button, Checkbox, Row, Col } from 'antd'
-const FormItem = Form.Item
+import {isLogin} from 'src/utils/auth'
+import {Form} from 'antd'
 import styles from './index.less'
-const appConfig = require('src/config/app')
+import Child from 'src/components/Child'
+import {findDOMNode} from 'react-dom'
+
+const FormItem = Form.Item
+const appConfig = require('src/config/app');
 
 
 const mapStateToProps = (state) => {
-  debugger
-  return {
-    auth: state.auth.toJS()
-  }
+    debugger
+    return {
+        auth: state.auth.toJS()
+    }
 }
 const mapDispatchToProps = dispatch => {
-  debugger
-  return {
-    actions: bindActionCreators(Actions, dispatch)
-  }
+    debugger
+    return {
+        actions: bindActionCreators(Actions, dispatch)
+    }
 }
 
 @Form.create()
 class Login extends Component {
-  constructor(props) {
-    debugger
-    super(props)
+    constructor(props) {
+        console.log()
+        debugger
+        super(props)
+        this.state = {
+            child1: {},
+            child2: {}
+        }
 
-  }
-
-  static propTypes = {
-
-    auth: PropTypes.object.isRequired,
-
-  };
-
-  componentDidMount() {
-    const { actions, auth } = this.props
-    if (auth.user === null) {
-      actions.getUserInfo()
     }
-  }
-  componentWillMount() {
-    // const { actions} = this.props
-    if (isLogin()) {
-      try {
-        window.location.href = '/'
-      } catch (err) {
-        console.log('忽略服务端渲染,组件检查的时候window is not defined')
-      }
+
+    static propTypes = {
+
+        auth: PropTypes.object.isRequired,
+
+    };
+
+    componentDidMount() {
+
     }
-  }
 
-  componentWillReceiveProps(nextProps) {
-    const { globalVal } = this.props
-    if (globalVal.styleMode !== nextProps.globalVal.styleMode) {
-      document.body.className = nextProps.globalVal.styleMode
+    componentWillMount() {
+
     }
-  }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    getChild1(dom){
+        debugger
+        // this.setState({
+        //     child1:dom
+        // });
+    }
+    getChild2(dom){
+        debugger
+        // this.setState({
+        //     child2:dom
+        // });
+    }
 
 
+    render() {
+        debugger
 
-  render() {
-debugger
+        return (
+            <div className={styles.loginCard}>
+                <Child ref={this.getChild1.bind(this)}/>
+                <div  ref={this.getChild2.bind(this)}>111</div>
+            </div>
 
-    return (
-      <div className={styles.loginCard} >
-
-      </div>
-
-    )
-  }
+        )
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login)
