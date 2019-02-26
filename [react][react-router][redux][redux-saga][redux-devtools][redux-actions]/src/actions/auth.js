768a38b372b0bb2fa542fff6a3c74cc6c6b90221
李俊 <lijun@lijundeMacBook-Air.local>
2019-02-26 17:35:25 +0800

    update

--- [react][react-router][redux][redux-saga][redux-devtools][redux-actions]/src/actions/auth.js
+++ [react][react-router][redux][redux-saga][redux-devtools][redux-actions]/src/actions/auth.js
@@ -3,12 +3,14 @@ import {push} from 'connected-react-router'
 import {saveCookie, getCookie, signOut} from 'src/utils/auth'
 import * as api from 'src/api'
 import AppConfig from 'src/config/app'
+import { createAction } from 'redux-actions';
 
 
-export const localLogin = function () {
-    return {
-        type: 'ABC',
-        captchaUrl: '666'
-    }
-}
+// export const localLogin = function () {
+//     return {
+//         type: 'ABC',
+//         captchaUrl: '666'
+//     }
+// }
 
+export const localLogin=createAction('ABC');
